d7b31ec2b5500b4c5a80630bdc893e4f3fd96074
Tom Hughes <tom@compton.nu>
2020-04-30 14:06:05 +0100

    Test git directly

--- cookbooks/test/metadata.rb
+++
@@ -1,7 +0,0 @@
-name              "test"
-maintainer        "OpenStreetMap Administrators"
-maintainer_email  "admins@openstreetmap.org"
-license           "Apache-2.0"
-description       "Test"
-
-version           "1.0.0"
